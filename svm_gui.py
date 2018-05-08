0e539f9b254f3eb17482c6348ac48e1725c42bef
Unknown <no.mercy.cs573@gmail.com>
2018-05-08 15:23:09 -0500

    gui
    ggg

--- svm_gui.py
+++ svm_gui.py
@@ -5,6 +5,7 @@ import sys
 from tkinter import messagebox
 from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfilename
+from funciones import *
 from svm_model import *
 
 root = tk.Tk()
@@ -12,18 +13,19 @@ root = tk.Tk()
 root.title("SVM")
 
 width = 500
-height = 350
+height = 400
 x_offset = 50
 y_offset = 100
 
 root.geometry("%dx%d+%d+%d" % (width, height,x_offset,y_offset))
 
 def OpenFile(label):
+    global name
     name = askopenfilename(initialdir="C:/Users/Batman/Documents/Programming/tkinter/",
                            filetypes =(("All Files","*.*"),("Text File", "*.txt")),
                            title = "Choose a file.")
     image2 = Image.open(name)
-    #displayImage = tk.PhotoImage(file = name)
+    image2 = image2.resize((250, 250), Image.ANTIALIAS)
     displayImage = ImageTk.PhotoImage(image2)
     label.configure(image = displayImage)
     label.image = displayImage
@@ -31,9 +33,14 @@ def OpenFile(label):
 
 
 def predict():
+    image_t = cv2.imread(name)
+    image_t = cv2.resize(image_t,(256,256))
+    image_t = resize(image_t)
+    image_t_fv = [feature_vector_4_img(process_image(image_t))]
+    out_put = clf.predict(image_t_fv)
     textBoxResult.config(state=NORMAL)
     textBoxResult.delete("1.0",END)
-    textBoxResult.insert(END,"111")
+    textBoxResult.insert(END,str(out_put[0]))
     textBoxResult.config(state=DISABLED)
 
 
@@ -49,11 +56,11 @@ label.place(x=100,y=50)
 
 #Load Button
 loadButton = tk.Button(frame,height=1, width=10,text="...",command = lambda: OpenFile(label))
-loadButton.place(x=100,y=310)
+loadButton.place(x=100,y=350)
 
 #Predict Button
 predictButton = tk.Button(frame,height=1, width=10,text="predict",command = predict)
-predictButton.place(x=300,y=310)
+predictButton.place(x=300,y=350)
 
 #TextBoxResult
 textBoxResult = tk.Text(frame,height = 1, width=8)
